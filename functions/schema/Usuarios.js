dfd6072af9e4e6d405cabd43e43f8c640e258a67
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2021-04-10 13:51:01 -0500

    ADD: Agrega schemas para usuarios y vacantes

--- functions/schema/Usuarios.js
+++ functions/schema/Usuarios.js
@@ -1,7 +1,74 @@
 const joi = require('joi');
 
 const Usuarios = (() => {
-    return {}
+    
+    const schemaContentLetters = joi.string().regex(/[a-zA-Z]+/);
+
+    const schemaContentNumbers = joi.string().regex(/\d+/);
+
+    const schemaCreateUser = joi.object({
+        nombre_user: joi.string().min(1).max(35).required(),
+        apellido_p_user: joi.string().min(1).max(35).required(),
+        apellido_m_user: joi.string().min(1).max(35).required(),
+        direccion_user: joi.string().min(1).max(150).required(),
+        correo_user: joi.string().min(1).max(70).email().required(),
+        password_user: joi.string().min(1).max(150).required(),
+        tipo_user: joi.string().min(1).max(10).allow('invited', 'manager', 'executive').required(),
+        access_to_user: joi.string().min(1).required()
+    });
+
+    const schemaLogin = joi.object({
+        password_user: joi.string().min(1).max(150).required()
+    });
+
+    const schemaUpdateUser = joi.object({
+        nombre_user: joi.string().min(1).max(35).required(),
+        apellido_p_user: joi.string().min(1).max(35).required(),
+        apellido_m_user: joi.string().min(1).max(35).required(),
+        direccion_user: joi.string().min(1).max(150).required(),
+        correo_user: joi.string().min(1).max(70).email().required(),
+        tipo_user: joi.string().min(1).max(10).allow('invited', 'manager', 'executive').required(),
+        access_to_user: joi.string().min(1).required(),
+        activo_user: joi.boolean().required()
+    });
+
+    const schemaUpdateDataGeneral = joi.object({
+        nombre_user: joi.string().min(1).max(35).required(),
+        apellido_p_user: joi.string().min(1).max(35).required(),
+        apellido_m_user: joi.string().min(1).max(35).required(),
+        direccion_user: joi.string().min(1).max(150).required()
+    });
+
+    const schemaUpdateEmail = joi.object({
+        correo_user: joi.string().min(1).max(70).email().required(),
+        password_user: joi.string().min(1).max(150).required()
+    });
+
+    const schemaUpdatePassword = joi.object({
+        password_user: joi.string().min(1).max(150).required(),
+        new_password_user: joi.string().min(1).max(150).required()
+    });
+
+    const schemaUpdateRecovery = joi.object({
+        recovery_code_user: joi.string().min(1).max(50).required()
+    });
+
+    const schemaUpdateStatus = joi.object({
+        activo_user: joi.boolean().required()
+    });
+
+    return {
+        schemaContentLetters,
+        schemaContentNumbers,
+        schemaCreateUser,
+        schemaLogin,
+        schemaUpdateUser,
+        schemaUpdateDataGeneral,
+        schemaUpdateEmail,
+        schemaUpdatePassword,
+        schemaUpdateRecovery,
+        schemaUpdateStatus,
+    }
 })();
 
 module.exports = Usuarios;
--- functions/schema/Vacantes.js
+++ functions/schema/Vacantes.js
@@ -1,7 +1,22 @@
 const joi = require('joi');
 
 const Vacantes = (() => {
-    return {}
+
+    const schemaVacante = joi.object({
+        puesto_vacante: joi.string().min(1).required(),
+        requisitos_vacante: joi.string().min(1).required(),
+        disponible_vacante: joi.string().min(1).required()
+    });
+    
+    const schemaDisponible = joi.object({
+        disponible_vacante: joi.string().min(1).required()
+    });
+
+    return {
+        schemaVacante,
+        schemaDisponible,
+    }
+
 })();
 
 module.exports = Vacantes;
