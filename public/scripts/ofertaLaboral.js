83711733faccfc9a948485bc12fff62c57ad22e7
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2021-04-09 19:48:02 -0500

    ADD: Agrega formulario para vacante

---
+++ public/scripts/ofertaLaboral.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', function() {
+    const dondeface = document.querySelector("#dondeface");
+    const dondepagina = document.querySelector("#dondepagina");
+    const dondeinstagram = document.querySelector("#dondeinstagram");
+    const dondeamigo = document.querySelector("#dondeamigo");
+    const dondepublicidad = document.querySelector("#dondepublicidad");
+    const dondeotro = document.querySelector("#dondeotro");
+
+    const setInputOther = () => {
+        console.log(dondeotro.checked);
+
+        if (dondeotro.checked) {
+            $("#otroInput").show(300);
+        } else {
+            $("#otroInput").hide(200);
+        }
+    }
+
+    const loadEvents = () => {
+        dondeface.addEventListener('click', setInputOther);
+        dondepagina.addEventListener('click', setInputOther);
+        dondeinstagram.addEventListener('click', setInputOther);
+        dondeamigo.addEventListener('click', setInputOther);
+        dondepublicidad.addEventListener('click', setInputOther);
+        dondeotro.addEventListener('click', setInputOther);
+    }
+
+    const main = () => {
+        setInputOther();
+        loadEvents();
+    }
+
+    main();
+
+});
