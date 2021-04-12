c44674a44773422084f86ed855132b7ec482c128
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2021-04-12 10:46:52 -0500

    REFACTRO: Mejora interfaz de oferta laboral

--- public/scripts/ofertaLaboral.js
+++ public/scripts/ofertaLaboral.js
@@ -5,6 +5,11 @@ window.addEventListener('load', function() {
     const dondeamigo = document.querySelector("#dondeamigo");
     const dondepublicidad = document.querySelector("#dondepublicidad");
     const dondeotro = document.querySelector("#dondeotro");
+    const btnShowForm = document.querySelector("#btn-show-form");
+    const spanFillForm = document.querySelector("#spanFillForm");
+    const contentFillBtn = document.querySelector("#contentFillBtn");
+
+    let showForm = false;
 
     const setInputOther = () => {
         if (dondeotro.checked) {
@@ -21,6 +26,18 @@ window.addEventListener('load', function() {
         dondeamigo.addEventListener('click', setInputOther);
         dondepublicidad.addEventListener('click', setInputOther);
         dondeotro.addEventListener('click', setInputOther);
+        btnShowForm.addEventListener('click', function() {
+            if (showForm) {
+                spanFillForm.classList.remove('icofont-collapse');
+                spanFillForm.classList.add('icofont-tasks');
+                contentFillBtn.innerHTML = 'Rellenar formulario';
+            } else {
+                spanFillForm.classList.remove('icofont-tasks');
+                spanFillForm.classList.add('icofont-collapse');
+                contentFillBtn.innerHTML = 'Minimizar formulario';
+            }
+            showForm = !showForm;
+        });
     }
 
     const main = () => {
