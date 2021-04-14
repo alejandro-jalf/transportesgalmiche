eba02f438817f8dd75fb2759172de21676d7a21e
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2021-04-14 10:46:37 -0500

    ADD: Libera rutas de vacantes

--- functions/routers/Vacantes.js
+++ functions/routers/Vacantes.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
+const {
+    getAllVacancies,
+    createVacancy,
+    updateVacancy,
+    updateAvailableVacancy,
+    deleteVacancy,
+} = require('../services');
 
 router.route("/v1/vacantes").get(async (req, res) => {
-    const { status, response } = await getAllVancantes();
+    const { status, response } = await getAllVacancies();
     res.status(status).json(response);
 });
 
@@ -12,7 +19,27 @@ router.route("/v1/vacantes/disponibles").get(async (req, res) => {
 
 router.route("/v1/vacantes").post(async (req, res) => {
     const { body } = req;
-    const { status, response } = await sendMail(body);
+    const { status, response } = await createVacancy(body);
+    res.status(status).json(response);
+});
+
+router.route("/v1/vacantes/:puesto_vacante").put(async (req, res) => {
+    const { body, params } = req;
+    const { puesto_vacante } = params;
+    const { status, response } = await updateVacancy(puesto_vacante, body);
+    res.status(status).json(response);
+});
+
+router.route("/v1/vacantes/:puesto_vacante/disponible").put(async (req, res) => {
+    const { body, params } = req;
+    const { puesto_vacante } = params;
+    const { status, response } = await updateAvailableVacancy(puesto_vacante, body);
+    res.status(status).json(response);
+});
+
+router.route("/v1/vacantes/:puesto_vacante").delete(async (req, res) => {
+    const { puesto_vacante } = req.params;
+    const { status, response } = await deleteVacancy(puesto_vacante);
     res.status(status).json(response);
 });
 
