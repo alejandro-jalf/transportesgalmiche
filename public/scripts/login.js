54b7f0277d1c966931de6b107bbd6b0adecfe955
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2021-05-07 18:03:11 -0500

    Agrega pestaña de logueo

---
+++ public/scripts/login.js
@@ -0,0 +1,97 @@
+var applogin = new Vue({
+    el: '#app',
+    data() {
+        return {
+            viewPassword: false,
+            loading: true,
+            user: '',
+            password: '',
+            alertDialog: {
+                title: 'Advertencia',
+                message: 'Mensage',
+                headerColor: 'text-white',
+                headerBackground: 'bg-warning',
+            }
+        }
+    },
+    computed: {
+        typeInput() {
+            return this.viewPassword ? 'text' : 'password'
+        }
+    },
+    methods: {
+        focusPassword() {
+            this.$refs.inputPassword.focus();
+        },
+        showAlertDialog(message = '', title = 'Advertencia', hColor = 'text-white', hBackground = 'bg-warning') {
+            this.alertDialog.message = message; 
+            this.alertDialog.title = title; 
+            this.alertDialog.headerColor = hColor; 
+            this.alertDialog.headerBackground = hBackground;
+            $('#exampleModal').modal('show');
+        },
+        validateData() {
+            if (this.user.trim() === '') {
+                this.showAlertDialog('Necesita ingresar un correo electronico')
+                return false;
+            }
+            if (this.password.trim() === '') {
+                this.showAlertDialog('Necesita ingresar su contraseña')
+                return false;
+            }
+            return true;
+        },
+        async initSesion() {
+            console.log('Inicia sesion');
+            if (this.validateData()) {
+                const urlApi = '';
+                try {
+                    const response = axios({
+                        method: 'post',
+                        url: urlApi,
+                        data: {
+                            correo_user: this.user.trim(),
+                            password_user: this.password.trim(),
+                        }
+                    })
+                    
+                    $('#loading').hide();
+                    if (response.data.success) {
+                        this.showAlertDialog(
+                            response.data.message,
+                            'Exito al iniciar sesion',
+                            'text-white',
+                            'bg-success'
+                        );
+                    } else {
+                        this.showAlertDialog(
+                            response.data.message,
+                            'Advertencia  al iniciar sesion',
+                            'text-white',
+                            'bg-warning'
+                        );
+                    }
+                    
+                    $('#loading').hide();
+                } catch (error) {
+                    console.log(error);
+                    if (error.response) {
+                        this.showAlertDialog(
+                            response.data.message,
+                            'Error al iniciar sesion',
+                            'text-white',
+                            'bg-danger'
+                        );
+                    } else {
+                        this.showAlertDialog(
+                            'Error inesperado intentelo mas tarde',
+                            'Error al iniciar sesion',
+                            'text-white',
+                            'bg-danger'
+                        );
+                    }
+                }
+            }
+        },
+    },
+})
